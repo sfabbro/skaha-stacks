abd4fd54164725a8b0c2edf57d59c79b67e23bc9
Sébastien Fabbro <sebfabbro@gmail.com>
2022-04-25 17:46:13 +0000

    cleanup for new code

--- base/README.md
+++
@@ -1,3 +0,0 @@
-# skaha-stack
-
-skaha-stack is a set of jupyter containers for the CANFAR science platform based on the jupyter docker stacks.
--- build.sh
+++ build.sh
@@ -1,43 +1,21 @@
 #!/bin/bash -eux
 
-DEVICES="cpu gpu"
-
 OWNER="images.canfar.net/skaha"
 TAG=$(date +%y.%m)
+CONTAINERS=( astroml astrotorch astroflow astroml-gpu astrotorch-gpu astroflow-gpu )
 
-deviceit() {
-    local dir=$1 device=${2}
-    [[ -e ${dir}/env.yml.in ]] && \
-	sed -e "s|%%DEVICE%%|${device}|g" ${dir}/env.yml.in > ${dir}/env.yml
-    [[ -e ${dir}/pinned.in ]] && \
-	sed -e "s|%%DEVICE%%|${device}|g" ${dir}/pinned.in > ${dir}/pinned
-    if [[ ${device} == cu ]]; then
-	sed -e "s|#- cudatoolkit|- cudatoolkit|" \
-	    -e "s|#- jupyterlab-nvdashboard|- jupyterlab-nvdashboard|" \
-	    -i ${dir}/env.yml
-    fi
-}
+pushd base
+docker build --rm --force-rm -t ${OWNER}/base:${TAG} --build-arg OWNER=${OWNER} .
+popd
 
-for d in ${DEVICES}; do
-    
-    if [[ ${d} == gpu ]]; then
-	c=cu
-	ext="-${d}"
-	deviceit rapids ${c}
-	make -C rapids  TAG=${TAG} IMAGE=${OWNER}/rapids${ext} build
-    else
-	c=cpu
-	ext=
-    fi
-    
-    deviceit base ${c}
-    make -C base  TAG=${TAG} IMAGE=${OWNER}/base${ext} build
-    deviceit astro ${c}
-    make -C astro TAG=${TAG} IMAGE=${OWNER}/astro${ext} DARGS="--build-arg BASE_CONTAINER=${OWNER}/base${ext}:${TAG}" build
-    deviceit ml ${c}
-    make -C ml    TAG=${TAG} IMAGE=${OWNER}/astroml${ext} DARGS="--build-arg BASE_CONTAINER=${OWNER}/astro${ext}:${TAG}" build
-    deviceit torch ${c}
-    make -C torch TAG=${TAG} IMAGE=${OWNER}/astrotorch${ext} DARGS="--build-arg BASE_CONTAINER=${OWNER}/astroml${ext}:${TAG}" build
-    deviceit flow ${c}
-    make -C flow  TAG=${TAG} IMAGE=${OWNER}/astroflow${ext} DARGS="--build-arg BASE_CONTAINER=${OWNER}/astroml${ext}:${TAG}" build
+mkdir -p build
+for container in "${CONTAINERS[@]}"; do
+    ./make-container.sh build/${container}
+    pushd build/${container}
+    docker build --rm --force-rm \
+	   -t ${OWNER}/${container}:${TAG} \
+	   --build-arg TAG=${TAG} \
+	   --build-arg OWNER=${OWNER} .
+    popd
+    docker push ${OWNER}/${container}:${TAG}
 done
